773952a6d961b81acca64d98eedbc76b1fed7167
Salim Allosh <Salim.Allosh.OFL@gmail.com>
2023-08-29 15:42:59 +0400

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:over_time/Features/Authentication/Page/auth_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:over_time/Core/Route/app_routes.dart';
+import 'package:over_time/Features/Authentication/blocs/bio_auth_cubit.dart';
 
 import 'Core/Theme/app_themes.dart';
 import 'Core/Dependincy Injection/dependency_injection.dart' as di;
@@ -8,5 +10,18 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.dependincyInit();
 
-  runApp(MaterialApp(theme: mainTheme, home: AuthPage()));
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<BioAuthCubit>(
+          create: (context) => BioAuthCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: mainTheme,
+        initialRoute: AppRoutes.authPageName,
+        routes: AppRoutes.routes,
+      ),
+    ),
+  );
 }
